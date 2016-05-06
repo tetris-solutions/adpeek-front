127329f83027b1bb19cb4cf8fa559249b2b7df07
Christian Amaral <darthcas@gmail.com>
2016-05-06 19:18:59 -0300

    dont use contextualize for params

--- src/components/OrdersSelector.js
+++ src/components/OrdersSelector.js
@@ -1,14 +1,13 @@
 import React from 'react'
 import Select from './Select'
 import map from 'lodash/map'
-import {contextualize} from './higher-order/contextualize'
 
 const {PropTypes} = React
 const orders = [{id: '0123', name: 'PI Outubro'}, {id: '0456', name: 'PI Março'}]
 
 export const OrdersSelector = React.createClass({
   displayName: 'Orders-Selector',
-  propTypes: {
+  contextTypes: {
     router: PropTypes.object,
     params: PropTypes.shape({
       company: PropTypes.string,
@@ -18,7 +17,7 @@ export const OrdersSelector = React.createClass({
     })
   },
   onChange ({target: {value}}) {
-    const {router, params: {company, workspace, folder}} = this.props
+    const {router, params: {company, workspace, folder}} = this.context
     const relative = `/company/${company}/workspace/${workspace}/folder/${folder}`
 
     if (value) {
@@ -29,7 +28,7 @@ export const OrdersSelector = React.createClass({
   },
   render () {
     return (
-      <Select name='order' value={this.props.params.order || ''} onChange={this.onChange}>
+      <Select name='order' value={this.context.params.order || ''} onChange={this.onChange}>
         <option value=''>Create order</option>
 
         {map(orders, ({id, name}) => (
@@ -41,4 +40,4 @@ export const OrdersSelector = React.createClass({
   }
 })
 
-export default contextualize(OrdersSelector, 'params', 'router')
+export default OrdersSelector
