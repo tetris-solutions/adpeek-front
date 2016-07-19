78b185141faf0fcd4311133b9b9bdc7548abd3f5
Christian Amaral <darthcas@gmail.com>
2016-07-19 15:16:06 -0300

    separate report daterange selector into its own component

--- src/components/ReportBuilder.js
+++ src/components/ReportBuilder.js
@@ -1,14 +1,12 @@
 import React from 'react'
 import Message from '@tetris/front-server/lib/components/intl/Message'
-import DateRange from './DateRange'
 import moment from 'moment'
-import Modal from './Modal'
+import ReportDateRange from './ReportDateRange'
 
 export const ReportBuilder = React.createClass({
   displayName: 'Report-Builder',
   getInitialState () {
     return {
-      isModalOpen: false,
       startDate: moment().startOf('week').subtract(7, 'days'),
       endDate: moment().startOf('week').subtract(1, 'days')
     }
@@ -16,14 +14,8 @@ export const ReportBuilder = React.createClass({
   onChangeRange ({startDate, endDate}) {
     this.setState({startDate, endDate})
   },
-  openModal () {
-    this.setState({isModalOpen: true})
-  },
-  closeModal () {
-    this.setState({isModalOpen: false})
-  },
   render () {
-    const {isModalOpen, startDate, endDate} = this.state
+    const {startDate, endDate} = this.state
 
     return (
       <div>
@@ -31,31 +23,13 @@ export const ReportBuilder = React.createClass({
           <div className='mdl-layout__header-row mdl-color--blue-grey-500'>
             <Message>reportBuilder</Message>
             <div className='mdl-layout-spacer'/>
-            <button className='mdl-button mdl-color-text--grey-100' onClick={this.openModal}>
-              <Message startDate={startDate.format('ddd D, MMM')} endDate={endDate.format('ddd D, MMM - YYYY')}>
-                dateRangeLabel
-              </Message>
-            </button>
-          </div>
-        </header>
 
-        {isModalOpen && (
-          <Modal provide={['messages', 'locales']}>
-            <h4>
-              <Message>reportRangeTitle</Message>
-            </h4>
-
-            <DateRange
-              messages={this.context.messages}
+            <ReportDateRange
               onChange={this.onChangeRange}
               startDate={startDate}
               endDate={endDate}/>
-
-            <button onClick={this.closeModal}>
-              close
-            </button>
-          </Modal>
-        )}
+          </div>
+        </header>
       </div>
     )
   }
---
+++ src/components/ReportDateRange.js
@@ -0,0 +1,70 @@
+import React from 'react'
+import Message from '@tetris/front-server/lib/components/intl/Message'
+import DateRange from './DateRange'
+import Modal from './Modal'
+
+const {PropTypes} = React
+
+const DateRangeModal = ({startDate, endDate, close, onChange}) => (
+  <Modal provide={['messages', 'locales']}>
+    <h4>
+      <Message>reportRangeTitle</Message>
+    </h4>
+
+    <DateRange
+      onChange={onChange}
+      startDate={startDate}
+      endDate={endDate}/>
+
+    <button className='mdl-button' onClick={close}>
+      close
+    </button>
+  </Modal>
+)
+
+DateRangeModal.displayName = 'Date-Range-Modal'
+DateRangeModal.propTypes = {
+  onChange: PropTypes.func,
+  close: PropTypes.func,
+  startDate: PropTypes.object,
+  endDate: PropTypes.object
+}
+
+const ReportDateRange = React.createClass({
+  displayName: 'Report-Date-Range',
+  getInitialState () {
+    return {isModalOpen: false}
+  },
+  propTypes: {
+    onChange: PropTypes.func,
+    startDate: PropTypes.object,
+    endDate: PropTypes.object
+  },
+  openModal () {
+    this.setState({isModalOpen: true})
+  },
+  closeModal () {
+    this.setState({isModalOpen: false})
+  },
+  render () {
+    const {startDate, endDate, onChange} = this.props
+
+    return (
+      <button className='mdl-button mdl-color-text--grey-100' onClick={this.openModal}>
+        <Message startDate={startDate.format('ddd D, MMM')} endDate={endDate.format('ddd D, MMM - YYYY')}>
+          dateRangeLabel
+        </Message>
+
+        {this.state.isModalOpen ? (
+          <DateRangeModal
+            close={this.closeModal}
+            onChange={onChange}
+            startDate={startDate}
+            endDate={endDate}/>
+        ) : null}
+      </button>
+    )
+  }
+})
+
+export default ReportDateRange
