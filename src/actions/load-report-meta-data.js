d23df6334b642817262bc5ce9170f59d1ace6784
Christian Amaral <darthcas@gmail.com>
2016-07-29 20:08:54 -0300

    report metadata change attributes before saving (exclude name, change id label)

--- src/actions/load-report-meta-data.js
+++ src/actions/load-report-meta-data.js
@@ -2,16 +2,34 @@ import {GET} from '@tetris/http'
 import {saveResponseTokenAsCookie} from '@tetris/front-server/lib/functions/save-token-as-cookie'
 import {getApiFetchConfig} from '@tetris/front-server/lib/functions/get-api-fetch-config'
 import {pushResponseErrorToState} from '@tetris/front-server/lib/functions/push-response-error-to-state'
+import omit from 'lodash/omit'
+import without from 'lodash/without'
+import set from 'lodash/set'
 
 function loadReportMetaData (platform, entity, config) {
   return GET(`${process.env.NUMBERS_API_URL}/meta?platform=${platform}&entity=${entity}`, config)
 }
 
+const excluded = [
+  'name',
+  'date_stop'
+]
+
 export function loadReportMetaDataAction (tree, platform, entity, token) {
   return loadReportMetaData(platform, entity, getApiFetchConfig(tree, token))
     .then(saveResponseTokenAsCookie)
     .then(response => {
-      tree.set(['reports', 'metaData', platform, entity], response.data)
+      const metaData = response.data
+      const entityNameMessage = `${entity.toLowerCase()}Entity`
+
+      metaData.attributes = omit(metaData.attributes, excluded)
+
+      set(metaData, 'attributes.id.name',
+        tree.get(['intl', 'messages', entityNameMessage]))
+
+      metaData.dimensions = without(metaData.dimensions, excluded)
+
+      tree.set(['reports', 'metaData', platform, entity], metaData)
       tree.commit()
 
       return response
