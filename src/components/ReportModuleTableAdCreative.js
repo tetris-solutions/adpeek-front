f82124307767d468795f1a268d33ddda6b4c6df9
Christian Amaral <darthcas@gmail.com>
2016-08-29 12:05:03 -0300

    ReportModule ~ style facebook creative ad

--- src/components/ReportModuleTableAdCreative.js
+++ src/components/ReportModuleTableAdCreative.js
@@ -1,14 +1,45 @@
+import csjs from 'csjs'
 import find from 'lodash/find'
 import React from 'react'
 
 import reportParamsType from '../propTypes/report-params'
 import {loadCreativeAction} from '../actions/load-creative'
 import {contextualize} from './higher-order/contextualize'
+import {styled} from './mixins/styled'
 
 const {PropTypes} = React
+const style = csjs`
+.post {
+  width: 256px;
+  background-color: white;
+  border-radius: 3px;
+  padding: 1em;
+  border: 1px solid rgb(200, 200, 200);
+  box-shadow: 1px 2px 2px rgba(0, 0, 0, 0.3);
+}
+.post > h2 {
+  color: #365899;
+  font-size: medium;
+  font-weight: bold;
+  line-height: 1.7em;
+  padding: 0;
+  margin: 0 0 .3em 0;
+}
+.post > img {
+  border: 1px solid rgb(220, 220, 220);
+  width: 98%;
+  height: auto;
+  margin: 0 auto;
+}
+.post > p {
+  font-size: small;
+  margin: .5em 0;
+  white-space: normal;
+}`
 
 const AdCreative = React.createClass({
   displayName: 'Ad-Creative',
+  mixins: [styled(style)],
   propTypes: {
     reportParams: reportParamsType,
     params: PropTypes.shape({
@@ -49,7 +80,13 @@ const AdCreative = React.createClass({
       return <span>{name}</span>
     }
 
-    return <pre>{JSON.stringify(creative, null, 2)}</pre>
+    return (
+      <div className={`${style.post}`}>
+        <h2>{creative.title}</h2>
+        <img src={creative.thumbnail} />
+        <p>{creative.body}</p>
+      </div>
+    )
   }
 })
 
