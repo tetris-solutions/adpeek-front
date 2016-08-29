edd1ca8133fdb7f9bd571f1aec171131613d93e7
Christian Amaral <darthcas@gmail.com>
2016-08-29 18:52:56 -0300

    change report result requests HTTP Method from GET to POST, dropping query-string dependency

--- src/actions/load-report-result.js
+++ src/actions/load-report-result.js
@@ -1,15 +1,15 @@
+import assign from 'lodash/assign'
 import isEqual from 'lodash/isEqual'
 import {getApiFetchConfig} from '@tetris/front-server/lib/functions/get-api-fetch-config'
 import {pushResponseErrorToState} from '@tetris/front-server/lib/functions/push-response-error-to-state'
 import {saveResponseTokenAsCookie} from '@tetris/front-server/lib/functions/save-token-as-cookie'
-import {GET} from '@tetris/http'
+import {POST} from '@tetris/http'
 
-import {assembleReportQuery} from '../functions/assemble-report-query'
 import {getDeepCursor} from '../functions/get-deep-cursor'
 import {isvalidReportQuery} from '../functions/is-valid-report-query'
 
 function loadReportModuleResult (query, config) {
-  return GET(`${process.env.NUMBERS_API_URL}?${assembleReportQuery(query)}`, config)
+  return POST(`${process.env.NUMBERS_API_URL}`, assign({body: query}, config))
 }
 
 const lastCall = {}
