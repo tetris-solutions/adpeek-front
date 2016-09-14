d6420dc60ed401093a06c721b3d3ba2bdf07e28b
Christian Amaral <darthcas@gmail.com>
2016-09-14 12:21:06 -0300

    fix expanded text ad component

--- src/components/AdGroupAd.js
+++ src/components/AdGroupAd.js
@@ -32,19 +32,29 @@ const doNothing = e => e.preventDefault()
 const AdGroupAd = ({
   id,
   headline,
+  headline_part_1,
+  headline_part_2,
   display_url,
+  description,
   description_1,
   description_2,
   final_urls
 }) => (
   <div className={`${style.wrapper}`}>
     <div className={`mdl-color--yellow-200 ${style.box}`}>
-      <h5>{headline}</h5>
+      <h5>
+        {headline_part_1 || headline}
+        {headline_part_2 ? <br/> : null}
+        {headline_part_2 || null}
+      </h5>
       <a href='#' onClick={doNothing}>
         {display_url}
       </a>
-      <div>{description_1}</div>
-      <div>{description_2}</div>
+      <div>{description || description_1}</div>
+
+      {description_2
+        ? <div>{description_2}</div>
+        : null}
     </div>
 
     {map(final_urls,
@@ -68,7 +78,10 @@ AdGroupAd.displayName = 'AdGroup-Ad'
 AdGroupAd.propTypes = {
   id: PropTypes.string,
   headline: PropTypes.string,
+  headline_part_1: PropTypes.string,
+  headline_part_2: PropTypes.string,
   display_url: PropTypes.string,
+  description: PropTypes.string,
   description_1: PropTypes.string,
   description_2: PropTypes.string,
   final_urls: PropTypes.array
