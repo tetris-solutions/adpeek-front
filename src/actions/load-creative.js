545a022eefd32acc0edff84c422bee55f271079c
Christian Amaral <darthcas@gmail.com>
2016-09-28 18:07:10 -0300

    update load-creative endpoint

--- src/actions/load-creative.js
+++ src/actions/load-creative.js
@@ -5,8 +5,8 @@ import {GET} from '@tetris/http'
 
 import {saveResponseData} from '../functions/save-response-data'
 
-function loadCreative (account, creative, config) {
-  return GET(`${process.env.ADPEEK_API_URL}/account/${account}/creative/${creative}`, config)
+function loadCreative (company, account, creative, config) {
+  return GET(`${process.env.ADPEEK_API_URL}/company/${company}/account/${account}/creative/${creative}`, config)
 }
 
 function one (action) {
@@ -24,7 +24,7 @@ function one (action) {
 }
 
 export const loadCreativeAction = one((tree, company, account, creative) =>
-  loadCreative(account, creative, getApiFetchConfig(tree))
+  loadCreative(company, account, creative, getApiFetchConfig(tree))
     .then(saveResponseTokenAsCookie)
     .then(saveResponseData(tree, [
       'user',
