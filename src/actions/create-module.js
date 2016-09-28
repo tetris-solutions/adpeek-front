33f7f6150e0b05232f8e07e1ccfbaab565f17b8a
Christian Amaral <darthcas@gmail.com>
2016-09-28 15:44:48 -0300

    change create-module endpoint

--- src/actions/create-module.js
+++ src/actions/create-module.js
@@ -6,21 +6,24 @@ import {POST} from '@tetris/http'
 
 import {getDeepCursor} from '../functions/get-deep-cursor'
 
-function createModule (company, report, module, config) {
-  return POST(`${process.env.ADPEEK_API_URL}/company/${company}/report/${report}/module`,
+function createModule (workspace, report, module, config) {
+  return POST(`${process.env.ADPEEK_API_URL}/workspace/${workspace}/report/${report}/module`,
     assign({body: module}, config))
 }
 
 export function createModuleReportAction (tree, {company, workspace, folder, report}, module) {
-  const path = ['user', ['companies', company]]
+  const path = [
+    'user',
+    ['companies', company],
+    ['workspaces', workspace]
+  ]
 
-  if (workspace) path.push(['workspaces', workspace])
   if (folder) path.push(['folders', folder])
 
   path.push(['reports', report])
   path.push('modules')
 
-  return createModule(company, report, module, getApiFetchConfig(tree))
+  return createModule(workspace, report, module, getApiFetchConfig(tree))
     .then(saveResponseTokenAsCookie)
     .then(response => {
       path.push(response.data.id)
