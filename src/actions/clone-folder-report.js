70debae55e270e4b5c1cdd75448c548ff8052097
Christian Amaral <darthcas@gmail.com>
2016-09-28 15:49:11 -0300

    change clone-folder-report endpoint

--- src/actions/clone-folder-report.js
+++ src/actions/clone-folder-report.js
@@ -4,13 +4,13 @@ import {saveResponseTokenAsCookie} from '@tetris/front-server/lib/functions/save
 import {getApiFetchConfig} from '@tetris/front-server/lib/functions/get-api-fetch-config'
 import {pushResponseErrorToState} from '@tetris/front-server/lib/functions/push-response-error-to-state'
 
-function cloneFolderReport (company, folder, report, config) {
-  return PUT(`${process.env.ADPEEK_API_URL}/company/${company}/folder/${folder}/report/${report.id}/clone`,
+function cloneFolderReport (folder, report, config) {
+  return PUT(`${process.env.ADPEEK_API_URL}/folder/${folder}/report/${report.id}/clone`,
     assign({body: report}, config))
 }
 
 export function cloneFolderReportAction (tree, company, workspace, folder, report) {
-  return cloneFolderReport(company, folder, report, getApiFetchConfig(tree))
+  return cloneFolderReport(folder, report, getApiFetchConfig(tree))
     .then(saveResponseTokenAsCookie)
     .catch(pushResponseErrorToState(tree))
 }
