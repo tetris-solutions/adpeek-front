4a7cbe0711f580b00b6464d69991a2cf9e6cd517
Christian Amaral <darthcas@gmail.com>
2016-10-07 15:30:51 -0300

    stop sending http logs to logstash

--- src/server.js
+++ src/server.js
@@ -1,7 +1,6 @@
 import path from 'path'
 import {createServer} from '@tetris/front-server/lib/server'
 import {setAppRoutes} from './routes/express'
-import {httpLogStream} from './logger'
 import defaultState from './default-state'
 import {getRoutes} from './routes/ui'
 import HTML from './components/HTML'
@@ -10,7 +9,6 @@ const config = {
   getWebpackConfig () {
     return require('./webpack.config')
   },
-  httpLogStream,
   defaultState,
   publicPath: path.resolve(__dirname, '..', 'public'),
   messagesFile: path.resolve(__dirname, 'messages', 'index.js'),
