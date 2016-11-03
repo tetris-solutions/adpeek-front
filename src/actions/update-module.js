12bfbf80b77b23306f240ccd4912034ebf4ba85a
Christian Amaral <darthcas@gmail.com>
2016-11-03 11:38:58 -0200

    adapt update-module

--- src/actions/update-module.js
+++ src/actions/update-module.js
@@ -3,28 +3,29 @@ import debounce from 'lodash/debounce'
 import omit from 'lodash/omit'
 import {saveResponseTokenAsCookie, getApiFetchConfig, pushResponseErrorToState} from 'tetris-iso/utils'
 import {PUT} from '@tetris/http'
-
+import compact from 'lodash/compact'
+import {inferLevelFromParams} from '../functions/infer-level-from-params'
 import {getDeepCursor} from '../functions/get-deep-cursor'
 
-function updateModule (module, folder, config) {
-  return PUT(`${process.env.ADPEEK_API_URL}/folder/${folder}/module/${module.id}`,
+function updateModule (module, level, id, config) {
+  return PUT(`${process.env.ADPEEK_API_URL}/${level}/${id}/module/${module.id}`,
     assign({body: module}, config))
 }
 
 function sendModuleChanges (tree, params, moduleId) {
-  const cursorPath = getDeepCursor(tree, [
+  const cursorPath = getDeepCursor(tree, compact([
     'user',
     ['companies', params.company],
-    ['workspaces', params.workspace],
-    ['folders', params.folder],
+    params.workspace && ['workspaces', params.workspace],
+    params.folder && ['folders', params.folder],
     ['reports', params.report],
     'modules',
     moduleId
-  ])
-
+  ]))
+  const level = inferLevelFromParams(params)
   const moduleChanges = omit(tree.get(cursorPath), 'result', 'query', 'isLoading')
 
-  updateModule(moduleChanges, params.folder, getApiFetchConfig(tree))
+  updateModule(moduleChanges, level, params[level], getApiFetchConfig(tree))
     .then(saveResponseTokenAsCookie)
     .catch(pushResponseErrorToState(tree))
 }
@@ -32,15 +33,15 @@ function sendModuleChanges (tree, params, moduleId) {
 const persist = debounce(sendModuleChanges, 1000)
 
 export function updateModuleAction (tree, params, moduleId, moduleChanges, persistChanges = false) {
-  const cursorPath = getDeepCursor(tree, [
+  const cursorPath = getDeepCursor(tree, compact([
     'user',
     ['companies', params.company],
-    ['workspaces', params.workspace],
-    ['folders', params.folder],
+    params.workspace && ['workspaces', params.workspace],
+    params.folder && ['folders', params.folder],
     ['reports', params.report],
     'modules',
     moduleId
-  ])
+  ]))
 
   tree.merge(cursorPath, moduleChanges)
   tree.commit()
