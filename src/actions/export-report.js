92a05415226b07a27eeccef860b42d626eb0ef8b
Christian Amaral <darthcas@gmail.com>
2016-11-04 22:17:44 -0200

    fix export-report

--- src/actions/export-report.js
+++ src/actions/export-report.js
@@ -2,13 +2,13 @@ import {POST} from '@tetris/http'
 import assign from 'lodash/assign'
 import {saveResponseTokenAsCookie, getApiFetchConfig, pushResponseErrorToState} from 'tetris-iso/utils'
 
-function exportReport (workspace, type, report, config) {
-  return POST(`${process.env.ADPEEK_API_URL}/workspace/${workspace}/report/${report.id}/export?type=${type}`,
+function exportReport (company, type, report, config) {
+  return POST(`${process.env.ADPEEK_API_URL}/company/${company}/report/${report.id}/export?type=${type}`,
     assign({body: report}, config))
 }
 
-export function exportReportAction (tree, {workspace}, type, report) {
-  return exportReport(workspace, type, report, getApiFetchConfig(tree))
+export function exportReportAction (tree, {company}, type, report) {
+  return exportReport(company, type, report, getApiFetchConfig(tree))
     .then(saveResponseTokenAsCookie)
     .catch(pushResponseErrorToState(tree))
 }
