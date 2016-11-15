207a82c3365a1f5e116a0ad1e69d9cb4baa86cda
Christian Amaral <darthcas@gmail.com>
2016-11-15 13:12:00 -0200

    implements order activeOnly filter

--- src/components/Orders.js
+++ src/components/Orders.js
@@ -13,12 +13,15 @@ import {Container} from './ThumbLink'
 import SubHeader, {SubHeaderButton} from './SubHeader'
 import Page from './Page'
 import Order from './OrderThumb'
+import Switch from './Switch'
+
 const {PropTypes} = React
 const cleanStr = str => trim(deburr(lowerCase(str)))
 
 export const Orders = React.createClass({
   displayName: 'Orders',
   contextTypes: {
+    moment: PropTypes.func.isRequired,
     location: PropTypes.object,
     params: PropTypes.shape({
       company: PropTypes.string,
@@ -31,16 +34,26 @@ export const Orders = React.createClass({
   },
   getInitialState () {
     return {
-      searchValue: ''
+      searchValue: '',
+      activeOnly: true
     }
   },
   onChange (searchValue) {
     this.setState({searchValue})
   },
+  onSwitch ({target: {checked: activeOnly}}) {
+    this.setState({activeOnly})
+  },
   render () {
+    const {activeOnly} = this.state
     const searchValue = cleanStr(this.state.searchValue)
-    const {location, params} = this.context
-    const {orders} = this.props
+    const {location, params, moment} = this.context
+    const today = moment().format('YYYY-MM-DD')
+
+    const orders = activeOnly
+      ? filter(this.props.orders, ({start, end}) => start <= today && end >= today)
+      : this.props.orders
+
     const matchesSearchTerm = ({name, folder_name, workspace_name}) => (
       includes(cleanStr(name), searchValue) ||
       includes(cleanStr(folder_name), searchValue) ||
@@ -68,6 +81,14 @@ export const Orders = React.createClass({
           </SubHeader>
           <Page>
             <Container>
+              <span style={{float: 'right'}}>
+                <Switch
+                  checked={this.state.activeOnly}
+                  name='activeOrdersOnly'
+                  label={<Message>filterActiveOnly</Message>}
+                  onChange={this.onSwitch}/>
+              </span>
+
               <h5>
                 <Message>orders</Message>
               </h5>
