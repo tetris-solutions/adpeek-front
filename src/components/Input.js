efff2614556872841cdb9efa813a81938f8ce39a
Christian Amaral <darthcas@gmail.com>
2016-11-14 22:12:17 -0200

    progress with Input number

--- src/components/Input.js
+++ src/components/Input.js
@@ -3,6 +3,9 @@ import pick from 'lodash/pick'
 import trim from 'lodash/trim'
 import Message from 'tetris-iso/Message'
 import React from 'react'
+import assign from 'lodash/assign'
+import MaskedTextInput from 'react-text-mask'
+import createNumberMask from 'text-mask-addons/dist/createNumberMask'
 import TextMessage from 'intl-messageformat'
 
 const {PropTypes} = React
@@ -31,7 +34,8 @@ export const Input = React.createClass({
     error: PropTypes.string,
     label: PropTypes.string,
     name: PropTypes.string.isRequired,
-    onChange: PropTypes.func
+    onChange: PropTypes.func,
+    prefix: PropTypes.string
   },
   contextTypes: {
     messages: PropTypes.object,
@@ -39,7 +43,8 @@ export const Input = React.createClass({
   },
   getDefaultProps () {
     return {
-      type: 'text'
+      type: 'text',
+      prefix: ''
     }
   },
   getInitialState () {
@@ -53,31 +58,25 @@ export const Input = React.createClass({
       isFocused: false
     }
   },
+  toNumber (value) {
+    const {locales} = this.context
+
+    const cleanValue = locales === 'pt-BR' ? (
+      value.replace(/\./g, '') // strip thousand sep
+        .replace(',', '.') // translate decimal sep
+    ) : (
+      value.replace(/,/g, '') // strip thousand sep
+    )
+
+    return Number(cleanValue.replace(/[^0-9-.]/g, '')
+      .replace(/\D$/g, ''))
+  },
   getNumberError (input) {
     const {messages: {invalidInput, greaterThanMax, lessThanMin}, locales} = this.context
 
-    let cleanValue = input.value.replace(/[^\d.,-]/g, '')
+    const number = this.toNumber(input.value)
 
-    let str = ''
-
-    for (let i = 0; i < cleanValue.length; i++) {
-      const char = cleanValue[i]
-      const canIgnore = (char === '.' || char === ',' || char === '-') && char === str[i - 1]
-
-      if (!canIgnore) {
-        str += char
-      }
-    }
-
-    cleanValue = str.replace(/\D$/g, '')
-
-    if (!input.value) return
-
-    if (input.value !== cleanValue || String(Number(cleanValue)) !== cleanValue) {
-      return invalidInput
-    }
-
-    const number = Number(cleanValue)
+    if (isNaN(number)) return invalidInput
 
     if (input.max && number > Number(input.max)) {
       return new TextMessage(greaterThanMax, locales).format({max: input.max})
@@ -86,76 +85,91 @@ export const Input = React.createClass({
     if (input.min && number < Number(input.min)) {
       return new TextMessage(lessThanMin, locales).format({min: input.min})
     }
-  },
-  getGenericError (input) {
-    if (input.required && !trim(input.value)) {
-      return this.context.messages.requiredInput
-    }
-  },
-  handleNewValue (input, fn) {
-    let error = this.getGenericError(input)
 
-    if (!error && input.type === 'number') {
+    return null
+  },
+  onChange (e) {
+    const {onChange, type} = this.props
+    const input = e.target
+
+    let error = null
+
+    if (input.required && !trim(input.value)) {
+      error = this.context.messages.requiredInput
+    } else if (type === 'number') {
       error = this.getNumberError(input)
     }
-
-    error = error || null
 
     this.setState({
       error,
       value: input.value,
       isDirty: notEmptyString(input.value)
-    }, fn)
-  },
-  onChange (e) {
-    e.persist()
-
-    this.handleNewValue(e.target, () => {
-      const hasHardError = (
-        this.state.error &&
-        this.state.error !== this.context.messages.requiredInput
-      )
-
-      if (!hasHardError && this.props.onChange) {
-        this.props.onChange(e)
-      }
     })
+
+    if (!error && onChange) {
+      onChange({
+        target: {
+          name: input.name,
+          value: type === 'number'
+            ? this.toNumber(input.value)
+            : input.value
+        }
+      })
+    }
   },
-  onFocus (e) {
+  onFocus () {
     this.setState({isFocused: true})
   },
-  onBlur (e) {
+  onBlur () {
     this.setState({isFocused: false})
   },
+  isMask () {
+    return this.props.type === 'number'
+  },
   componentWillReceiveProps ({value, min, max}) {
-    if (value !== this.props.value || min !== this.props.min || max !== this.props.max) {
-      this.refs.input.value = value
-      this.refs.input.min = min
-      this.refs.input.max = max
-      this.handleNewValue(this.refs.input)
+    if (value !== this.props.value) {
+      this.setState({value})
     }
   },
   render () {
-    const {isDirty, isFocused} = this.state
+    const {value, isDirty, isFocused} = this.state
+    const {prefix, label} = this.props
+    const {locales} = this.context
     const error = this.state.error || this.props.error
-    const {label} = this.props
+
     const wrapperClasses = cx('mdl-textfield',
       label && 'mdl-textfield--floating-label',
       error && 'is-invalid',
       isDirty && 'is-dirty',
       isFocused && 'is-focused')
 
+    let Tag = 'input'
+
+    const inputProps = assign(pick(this.props, inputFields), {
+      ref: 'input',
+      value,
+      className: 'mdl-textfield__input',
+      onChange: this.onChange,
+      onBlur: this.onBlur,
+      onFocus: this.onFocus
+    })
+
+    if (this.isMask()) {
+      Tag = MaskedTextInput
+
+      inputProps.type = 'text'
+      inputProps.mask = createNumberMask({
+        prefix,
+        allowDecimal: true,
+        decimalSymbol: locales === 'pt-BR' ? ',' : '.',
+        thousandsSeparatorSymbol: locales === 'pt-BR' ? '.' : ','
+      })
+    }
+
     return (
       <div className={wrapperClasses}>
 
-        <input
-          {...pick(this.props, inputFields)}
-          ref='input'
-          value={this.state.value}
-          className='mdl-textfield__input'
-          onChange={this.onChange}
-          onBlur={this.onBlur}
-          onFocus={this.onFocus}/>
+        <Tag {...inputProps}/>
 
         {label && (
           <label className='mdl-textfield__label'>
