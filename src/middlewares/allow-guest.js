3e27328698b4237f7db8caf8f2f602ceb6739b3e
Christian Amaral <darthcas@gmail.com>
2016-11-21 23:48:52 -0200

    fix session loading

--- src/middlewares/allow-guest.js
+++ src/middlewares/allow-guest.js
@@ -1,17 +1,25 @@
+import qs from 'query-string'
+import assign from 'lodash/assign'
+
 const ONE_DAY = 1000 * 60 * 60 * 24
 
-export function allowGuestMiddleware ({query, user}, res, next) {
-  if (!user && query.tkn) {
-    const accessToken = new Buffer(query.tkn, 'base64').toString('ascii')
-    const domain = process.env.TOKEN_COOKIE_DOMAIN
-    const cookieName = process.env.TOKEN_COOKIE_NAME
+export function allowGuestMiddleware ({query, path, user}, res, next) {
+  if (user || !query.tkn) return next()
 
-    res.set('Authorization', `Bearer ${accessToken}`)
-    res.cookie(cookieName, accessToken, {
-      domain,
-      expires: new Date(Date.now() + ONE_DAY)
-    })
-  }
+  const accessToken = new Buffer(query.tkn, 'base64').toString('ascii')
+  const domain = process.env.TOKEN_COOKIE_DOMAIN
+  const cookieName = process.env.TOKEN_COOKIE_NAME
 
-  next()
+  res.set('Authorization', `Bearer ${accessToken}`)
+  res.cookie(cookieName, accessToken, {
+    domain,
+    expires: new Date(Date.now() + ONE_DAY)
+  })
+
+  // redirect the user just once so the guest session is loaded
+
+  query = assign({}, query)
+  query._t = Date.now()
+
+  res.redirect(path + qs.stringify(query))
 }
