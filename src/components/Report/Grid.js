87c05c7bec03e9d28f006f8dead4d73b25c5b5c3
Christian Amaral <darthcas@gmail.com>
2017-01-05 12:30:43 -0200

    grid optimizations

--- src/components/Report/Grid.js
+++ src/components/Report/Grid.js
@@ -5,8 +5,6 @@ import Module from './Module/Container'
 import GridLayout from 'react-grid-layout'
 import sizeMe from 'react-sizeme'
 
-const Grid = sizeMe()(props => <GridLayout width={props.size.width} {...props}/>)
-
 class ReportGrid extends React.PureComponent {
   render () {
     const {
@@ -18,27 +16,24 @@ class ReportGrid extends React.PureComponent {
       params,
       dispatch,
       metaData,
-      openModule
+      openModule,
+      width
     } = this.props
 
     return (
-      <div className='mdl-grid'>
-        <div className='mdl-cell mdl-cell--12-col'>
-          <Grid layout={layout} rowHeight={100} onLayoutChange={onLayoutChange}>
-            {map(modules, (module, index) =>
-              <div key={module.id} data-module-id={module.id} data-module-type={module.type}>
-                <Module
-                  params={params}
-                  dispatch={dispatch}
-                  module={module}
-                  editable={editMode}
-                  metaData={get(metaData, module.entity)}
-                  clone={cloneModule}
-                  editMode={openModule === module.id}/>
-              </div>)}
-          </Grid>
-        </div>
-      </div>
+      <GridLayout layout={layout} width={width} rowHeight={100} onLayoutChange={onLayoutChange}>
+        {map(modules, (module, index) =>
+          <div key={module.id} data-module-id={module.id} data-module-type={module.type}>
+            <Module
+              params={params}
+              dispatch={dispatch}
+              module={module}
+              editable={editMode}
+              metaData={get(metaData, module.entity)}
+              clone={cloneModule}
+              editMode={openModule === module.id}/>
+          </div>)}
+      </GridLayout>
     )
   }
 }
@@ -53,7 +48,32 @@ ReportGrid.propTypes = {
   params: React.PropTypes.object.isRequired,
   dispatch: React.PropTypes.func.isRequired,
   metaData: React.PropTypes.object.isRequired,
-  openModule: React.PropTypes.string
+  openModule: React.PropTypes.string,
+  width: React.PropTypes.number
 }
 
-export default ReportGrid
+const DynamicGrid = props => <ReportGrid width={props.size.width} {...props}/>
+
+DynamicGrid.propTypes = {
+  size: React.PropTypes.shape({
+    width: React.PropTypes.number
+  })
+}
+
+DynamicGrid.displayName = 'Dynamic-Grid'
+
+const WrappedGrid = sizeMe({
+  refreshMode: 'debounce'
+})(DynamicGrid)
+
+const HardGrid = props => (
+  <div className='mdl-grid'>
+    <div className='mdl-cell mdl-cell--12-col'>
+      <WrappedGrid {...props}/>
+    </div>
+  </div>
+)
+
+HardGrid.displayName = 'Hard-Grid'
+
+export default HardGrid
