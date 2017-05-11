b603675e331be34959bfcdec3494d82f4a2fb291
Christian Amaral <darthcas@gmail.com>
2017-05-11 18:54:03 -0300

    fixes in-place criteria update

--- src/actions/update-campaign-language.js
+++ src/actions/update-campaign-language.js
@@ -1,9 +1,13 @@
 import {PUT} from '@tetris/http'
-import forEach from 'lodash/forEach'
-import assign from 'lodash/assign'
-import keyBy from 'lodash/keyBy'
 import {saveResponseTokenAsCookie, getApiFetchConfig, pushResponseErrorToState} from 'tetris-iso/utils'
 import {getDeepCursor} from '../functions/get-deep-cursor'
+import concat from 'lodash/concat'
+import assign from 'lodash/assign'
+import filter from 'lodash/filter'
+import diff from 'lodash/difference'
+import map from 'lodash/map'
+import includes from 'lodash/includes'
+import keyBy from 'lodash/keyBy'
 
 function updateCampaignLanguage (id, languages, config) {
   return PUT(`${process.env.ADPEEK_API_URL}/campaign/${id}/criteria/language`, assign({body: languages}, config))
@@ -23,18 +27,32 @@ export function updateCampaignLanguageAction (tree, {company, workspace, folder,
         'criteria'
       ])
 
-      const campaignCriteria = keyBy(tree.get(criteriaPath), 'id')
+      const indexedLanguages = keyBy(languages, 'id')
+      const criteria = tree.get(criteriaPath)
+      const oldLanguageIds = map(filter(criteria, {type: 'LANGUAGE'}), 'id')
+      const newLanguageIds = map(languages, 'id')
 
-      forEach(languages, ({id, name, code}) => {
-        if (!campaignCriteria[id]) {
-          tree.push(criteriaPath, {
-            id,
-            language: name,
-            language_code: code,
-            type: 'LANGUAGE'
-          })
+      const removedIds = diff(oldLanguageIds, newLanguageIds)
+      const insertedIds = diff(newLanguageIds, oldLanguageIds)
+
+      const notInRemovedIds = ({id}) => !includes(removedIds, id)
+      const notRemoved = filter(criteria, notInRemovedIds)
+
+      function normalizedCriteria (id) {
+        const {name, code} = indexedLanguages[id]
+
+        return {
+          id,
+          language: name,
+          language_code: code,
+          type: 'LANGUAGE'
         }
-      })
+      }
+
+      tree.set(criteriaPath, concat(
+        notRemoved,
+        map(insertedIds, normalizedCriteria)
+      ))
 
       return response
     })
