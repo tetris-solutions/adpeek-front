cf07501ec0b1fc3ef28c5a7c8c0e34cac28caec3
Christian Amaral <darthcas@gmail.com>
2017-05-12 16:53:00 -0300

    implement campaign.network update biz rules

--- src/components/campaign/edit/Network.js
+++ src/components/campaign/edit/Network.js
@@ -5,10 +5,17 @@ import {Button, Submit} from '../../Button'
 import noop from 'lodash/noop'
 import pick from 'lodash/pick'
 import map from 'lodash/map'
+import omit from 'lodash/omit'
+import some from 'lodash/some'
 import Checkbox from '../../Checkbox'
 import {updateCampaignNetworkAction} from '../../../actions/update-campaign-network'
 import {networkNames, networkMessages} from '../adwords/Network'
 import forEach from 'lodash/forEach'
+
+const isDisplayCampaign = networks => (
+  networks.content_network &&
+  !some(omit(networks, 'content_network'))
+)
 
 class EditNetwork extends React.Component {
   static displayName = 'Edit-Network'
@@ -45,18 +52,20 @@ class EditNetwork extends React.Component {
   render () {
     const {messages} = this.context
     const {campaign: {details}, onSubmit: close} = this.props
+    const networks = pick(details, networkNames)
+    const readOnly = isDisplayCampaign(networks)
 
     return (
       <form onSubmit={this.onSubmit}>
         <div className='mdl-grid'>
           <div className='mdl-cell mdl-cell--12-col'>
-            {map(pick(details, networkNames), (checked, name) =>
+            {map(networks, (enabledForCampaign, name) =>
               <Checkbox
                 key={name}
                 name={name}
                 label={messages[networkMessages[name]]}
-                disabled={checked}
-                checked={checked}/>)}
+                disabled={readOnly || enabledForCampaign}
+                checked={enabledForCampaign}/>)}
           </div>
         </div>
 
