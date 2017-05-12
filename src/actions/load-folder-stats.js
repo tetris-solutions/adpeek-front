bef9ade7d270fc0f11bb0b3e2f7e4234c4168f6f
Christian Amaral <darthcas@gmail.com>
2017-05-12 11:01:19 -0300

    folder-stats updateMode

--- src/actions/load-folder-stats.js
+++ src/actions/load-folder-stats.js
@@ -2,19 +2,21 @@ import {GET} from '@tetris/http'
 import {mergeResponseData} from '../functions/save-response-data'
 import {getApiFetchConfig, saveResponseTokenAsCookie, pushResponseErrorToState} from 'tetris-iso/utils'
 
-function loadFolderStats (id, requestFreshStats, config) {
-  const qs = requestFreshStats ? '?fresh=true' : ''
+function loadFolderStats (id, updateMode, config) {
+  const qs = updateMode !== 'normal'
+    ? `?${updateMode}=true`
+    : ''
 
   return GET(`${process.env.ADPEEK_API_URL}/folder/${id}/stats${qs}`, config)
 }
 
-export function loadFolderStatsAction (tree, {company, workspace}, folder, requestFreshStats = false) {
+export function loadFolderStatsAction (tree, {company, workspace}, folder, updateMode = 'normal') {
   // @todo delete this
   if (window.sessionStorage.getItem('cardStats') !== 'enabled') {
-    return Promise.resolve()
+    updateMode = 'cached'
   }
 
-  return loadFolderStats(folder, requestFreshStats, getApiFetchConfig(tree))
+  return loadFolderStats(folder, updateMode, getApiFetchConfig(tree))
     .then(saveResponseTokenAsCookie)
     .then(mergeResponseData(tree, [
       'user',
