95b7fe82f6df43d42a40aea7f06bb059f6dd179a
Christian Amaral <darthcas@gmail.com>
2017-05-18 19:34:23 -0300

    implements proximity circle

--- src/components/campaign/edit/Proximity.js
+++ src/components/campaign/edit/Proximity.js
@@ -5,12 +5,19 @@ import AutoComplete from '../../maps/AutoComplete'
 import keys from 'lodash/keys'
 import Map from '../../maps/Map'
 import Marker from '../../maps/Marker'
+import Circle from '../../maps/Circle'
 import compact from 'lodash/compact'
 import join from 'lodash/join'
+import isNumber from 'lodash/isNumber'
 
 const unitAbbr = {
   KILOMETERS: 'Km',
   MILES: 'Miles'
+}
+
+const unitMultiplier = {
+  KILOMETERS: 1000,
+  MILES: 1609.34
 }
 
 function stringifyAddressComponents (addr) {
@@ -48,7 +55,8 @@ class EditProximity extends React.Component {
   }
 
   static defaultProps = {
-    unit: 'KILOMETERS'
+    unit: 'KILOMETERS',
+    radius: 10
   }
 
   state = {
@@ -84,6 +92,7 @@ class EditProximity extends React.Component {
 
   render () {
     const {lat, lng, radius, unit} = this.props
+    const validCoords = isNumber(lat) && isNumber(lng)
 
     return (
       <div className='mdl-grid'>
@@ -105,10 +114,18 @@ class EditProximity extends React.Component {
         </div>
         <div className='mdl-cell mdl-cell--12-col'>
           <Map>
-            {lat !== undefined && (
+            {validCoords && (
               <Marker
                 lat={lat}
                 lng={lng}
+                move={this.onMarkerMove}
+                centered/>)}
+
+            {validCoords && (
+              <Circle
+                lat={lat}
+                lng={lng}
+                radius={radius * unitMultiplier[unit]}
                 move={this.onMarkerMove}
                 centered/>)}
           </Map>
---
+++ src/components/maps/Circle.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import omit from 'lodash/omit'
+
+class Circle extends React.PureComponent {
+  static displayName = 'Circle'
+
+  static propTypes = {
+    lat: PropTypes.number,
+    lng: PropTypes.number,
+    radius: PropTypes.number,
+    strokeColor: PropTypes.string,
+    strokeOpacity: PropTypes.number,
+    strokeWeight: PropTypes.number,
+    fillColor: PropTypes.string,
+    fillOpacity: PropTypes.number
+  }
+
+  static contextTypes = {
+    map: PropTypes.object
+  }
+
+  static defaultProps = {
+    draggable: true,
+    strokeColor: '#920f0f',
+    strokeOpacity: 0.7,
+    strokeWeight: 2,
+    fillColor: '#0006f1',
+    fillOpacity: 0.3
+  }
+
+  componentDidMount () {
+    this.circle = new google.maps.Circle(this.getOptions())
+  }
+
+  componentWillUnmount () {
+    this.circle.setMap(null)
+  }
+
+  componentDidUpdate () {
+    this.circle.setOptions(this.getOptions())
+  }
+
+  getOptions () {
+    const options = omit(this.props, 'lat', 'lng')
+
+    options.center = new google.maps.LatLng(this.props.lat, this.props.lng)
+    options.map = this.context.map
+
+    return options
+  }
+
+  render () {
+    return null
+  }
+}
+
+export default Circle
