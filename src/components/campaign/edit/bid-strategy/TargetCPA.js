7f33580ca38e12f396ece322d65ee6c105d89a6b
Christian Amaral <darthcas@gmail.com>
2017-05-26 15:53:23 -0300

    disable edit of saved target CPA strategy

--- src/components/campaign/edit/bid-strategy/TargetCPA.js
+++ src/components/campaign/edit/bid-strategy/TargetCPA.js
@@ -4,19 +4,20 @@ import AnonymousStrategyToggle from './AnonymousStrategyToggle'
 import Input from '../../../Input'
 
 const TargetCPA = props => (
-  <AnonymousStrategyToggle {...props}>
+  <AnonymousStrategyToggle {...props}>{!props.strategyId && (
     <Input
       type='number'
       format='currency'
       name='targetCPA'
       label='targetCPA'
       onChange={({target: {value: targetCPA}}) => props.update({targetCPA})}
-      value={props.targetCPA}/>
+      value={props.targetCPA}/>)}
   </AnonymousStrategyToggle>
 )
 
 TargetCPA.displayName = 'Target-CPA'
 TargetCPA.propTypes = {
+  strategyId: PropTypes.string,
   targetCPA: PropTypes.number,
   update: PropTypes.func.isRequired
 }
