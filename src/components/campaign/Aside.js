c72ac08d95a2dc468222263ce8dd0a26f25de918
Christian Amaral <darthcas@gmail.com>
2017-06-12 13:05:47 -0300

    fix campaign aside button links

--- src/components/campaign/Aside.js
+++ src/components/campaign/Aside.js
@@ -5,6 +5,8 @@ import {Link} from 'react-router'
 import Message from 'tetris-iso/Message'
 import startsWith from 'lodash/startsWith'
 import {Navigation, NavBt, NavBts, Name} from '../Navigation'
+import qs from 'query-string'
+import isEmpty from 'lodash/isEmpty'
 
 class CampaignAside extends React.PureComponent {
   static displayName = 'Campaign-Aside'
@@ -26,15 +28,21 @@ class CampaignAside extends React.PureComponent {
 
   render () {
     const {params: {company, workspace, folder}, campaign} = this.props
-    const {location: {pathname}} = this.context
+    const {location: {query, pathname}} = this.context
 
     const folderUrl = `/company/${company}/workspace/${workspace}/folder/${folder}`
+    const campaignUrl = `${folderUrl}/campaign/${campaign.id}`
 
-    const creativesUrl = `${folderUrl}/campaign/${campaign.id}/creatives`
-    const editCreativesUrl = creativesUrl + '/edit'
+    let creativesUrl = `${campaignUrl}/creatives`
+    let editCreativesUrl = creativesUrl + '/edit'
 
     const inCreativesScreen = startsWith(pathname, creativesUrl)
     const editCreativesMode = pathname === editCreativesUrl
+
+    if (inCreativesScreen && !isEmpty(query)) {
+      creativesUrl = creativesUrl + '?' + qs.stringify(query)
+      editCreativesUrl = editCreativesUrl + '?' + qs.stringify(query)
+    }
 
     return (
       <Navigation icon='format_shapes'>
@@ -56,7 +64,7 @@ class CampaignAside extends React.PureComponent {
               <Message>newAdGroup</Message>
             </NavBt>)}
 
-          <NavBt tag={Link} to={editCreativesMode ? creativesUrl : folderUrl} icon='close'>
+          <NavBt tag={Link} to={editCreativesMode ? creativesUrl : (inCreativesScreen ? campaignUrl : folderUrl)} icon='close'>
             <Message>oneLevelUpNavigation</Message>
           </NavBt>
         </NavBts>
