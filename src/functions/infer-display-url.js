70b726b041864eecd30c3be68b0b2d8e8400066e
Christian Amaral <darthcas@gmail.com>
2017-06-13 17:04:14 -0300

    refactor infer-display-url

--- src/functions/infer-display-url.js
+++ src/functions/infer-display-url.js
@@ -1,18 +1,26 @@
-import startsWith from 'lodash/startsWith'
 import join from 'lodash/join'
 import compact from 'lodash/compact'
+import head from 'lodash/head'
+import isString from 'lodash/isString'
 
-const w3 = 'www.'
-const stripW3 = str => startsWith(str, w3) ? str.substr(w3.length) : str
+export function finalUrlsDomain (final_urls) {
+  const finalUrl = head(final_urls)
+
+  if (!isString(finalUrl)) return null
+
+  const urlParts = finalUrl
+    .replace(/.*?:\/\//g, '')
+    .split('/')
+
+  return head(urlParts)
+}
 
 export function inferDisplayUrl (final_urls, path_1, path_2) {
-  if (!final_urls || !final_urls[0]) return null
+  const domain = finalUrlsDomain(final_urls)
 
-  const path_0 = final_urls[0]
-    .replace(/.*?:\/\//g, '')
-    .split('/')[0]
+  if (!domain) return null
 
-  const url = join(compact([`www.${stripW3(path_0)}`, path_1, path_2]), '/')
+  const url = join(compact([domain, path_1, path_2]), '/')
 
   return url.replace(/\/$/g, '')
 }
