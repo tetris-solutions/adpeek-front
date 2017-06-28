db022eae3aecb5f65f541acb15b08b5b3f3d805e
Christian Amaral <darthcas@gmail.com>
2017-06-28 13:22:32 -0300

    fix action url

--- src/actions/update-campaign-user-list.js
+++ src/actions/update-campaign-user-list.js
@@ -3,7 +3,7 @@ import assign from 'lodash/assign'
 import {saveResponseTokenAsCookie, getApiFetchConfig, pushResponseErrorToState} from 'tetris-iso/utils'
 
 function updateCampaignUserLists (id, userLists, config) {
-  return PUT(`${process.env.ADPEEK_API_URL}/campaign/${id}/user-lists`, assign({body: userLists}, config))
+  return PUT(`${process.env.ADPEEK_API_URL}/campaign/${id}/criteria/user-list`, assign({body: userLists}, config))
 }
 
 export function updateCampaignUserListsAction (tree, {campaign}, userLists) {
