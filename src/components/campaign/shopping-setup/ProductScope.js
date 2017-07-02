b505d63ce1eaeb93279eb455da6a7b1ad4374367
Christian Amaral <darthcas@gmail.com>
2017-07-01 22:19:42 -0300

    implements new filter button

--- src/components/campaign/shopping-setup/ProductScope.js
+++ src/components/campaign/shopping-setup/ProductScope.js
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types'
 import Message from 'tetris-iso/Message'
 import map from 'lodash/map'
 import Selector, {dimensionTypeMap} from './Selector'
+import Form from '../../Form'
+import {Button, Submit} from '../../Button'
 import {loadProductCategoriesAction} from '../../../actions/load-product-categories'
 import assign from 'lodash/assign'
 import concat from 'lodash/concat'
@@ -10,6 +12,8 @@ import filter from 'lodash/filter'
 import keyBy from 'lodash/keyBy'
 import size from 'lodash/size'
 import findIndex from 'lodash/findIndex'
+import first from 'lodash/first'
+import isEmpty from 'lodash/isEmpty'
 
 const availableTypes = map(dimensionTypeMap, (d, type) => assign({}, d, {type}))
 
@@ -50,26 +54,8 @@ class ProductScope extends React.Component {
     )
   }
 
-  updateDimension = (index, changes) => {
-    this.setState({
-      dimensions: map(this.state.dimensions,
-        (d, i) =>
-          i === index
-            ? assign({}, d, changes)
-            : d)
-    })
-  }
-
-  removeDimension = (index) => {
-    const dimensions = concat(this.state.dimensions)
-
-    dimensions.splice(index, 1)
-
-    this.setState({dimensions})
-  }
-
-  listEnabledTypesFor = (dimensionType, mappedDimensions) => {
-    return filter(availableTypes, ({parent, type}, i) => {
+  listEnabledTypesFor (dimensionType, mappedDimensions) {
+    return filter(availableTypes, ({parent, type}) => {
       if (type !== dimensionType && mappedDimensions[type]) {
         return false
       }
@@ -84,14 +70,57 @@ class ProductScope extends React.Component {
     })
   }
 
+  getRemainingTypes () {
+    const selected = keyBy(this.state.dimensions, 'type')
+
+    return filter(availableTypes, ({type}) => !selected[type])
+  }
+
+  addDimension = () => {
+    const next = first(this.getRemainingTypes())
+
+    const dimensions = concat(this.state.dimensions, {
+      type: next.type,
+      ProductDimensionType: next.category,
+      value: null
+    })
+
+    this.setState({dimensions})
+  }
+
+  updateDimension = (index, changes) => {
+    this.setState({
+      dimensions: map(this.state.dimensions,
+        (d, i) => i === index
+          ? assign({}, d, changes)
+          : d)
+    })
+  }
+
+  removeDimension = (index) => {
+    const dimensions = concat(this.state.dimensions)
+
+    dimensions.splice(index, 1)
+
+    this.setState({dimensions})
+  }
+
+  save = () => {
+
+  }
+
   render () {
+    if (!this.metaDataReady()) {
+      return <Message>loadingCategories</Message>
+    }
+
     const {dimensions} = this.state
     const lastOne = size(dimensions) - 1
-    const mappedDimensions = keyBy(dimensions, 'type')
+    const mappedDimensions = keyBy(this.state.dimensions, 'type')
 
     return (
-      <div>
-        {this.metaDataReady() ? map(dimensions, (dimension, index) =>
+      <Form onSubmit={this.save}>
+        {map(dimensions, (dimension, index) =>
           <Selector
             key={index}
             {...dimension}
@@ -101,9 +130,21 @@ class ProductScope extends React.Component {
             remove={this.removeDimension}
             dimensions={mappedDimensions}
             types={this.listEnabledTypesFor(dimension.type, mappedDimensions)}
-            categories={this.props.folder.productCategories}/>)
-          : <Message>loadingCategories</Message>}
-      </div>
+            categories={this.props.folder.productCategories}/>)}
+
+        <hr/>
+        <div>
+          <Button
+            className='mdl-button'
+            disabled={isEmpty(this.getRemainingTypes())}
+            onClick={this.addDimension}>
+            <Message>newFilter</Message>
+          </Button>
+          <Submit className='mdl-button' style={{float: 'right'}}>
+            <Message>save</Message>
+          </Submit>
+        </div>
+      </Form>
     )
   }
 }
