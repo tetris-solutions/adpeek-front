6a8589160d4b185d510e86634036346f7e4e56fd
Christian Amaral <darthcas@gmail.com>
2017-07-05 15:26:01 -0300

    mount partition tree

--- src/components/campaign/shopping-setup/AdGroup.js
+++ src/components/campaign/shopping-setup/AdGroup.js
@@ -3,6 +3,21 @@ import PropTypes from 'prop-types'
 import {node} from '../../higher-order/branch'
 import {loadAdGroupPartitionsAction} from '../../../actions/load-adgroup-partitions'
 import once from 'lodash/once'
+import forEach from 'lodash/forEach'
+import filter from 'lodash/filter'
+import assign from 'lodash/assign'
+import find from 'lodash/find'
+
+const make = node => assign({children: {}}, node)
+
+const mount = (partitions, node) =>
+  forEach(filter(partitions, {parent: node.id}), leaf => {
+    leaf = make(leaf)
+
+    node.children[leaf.id] = leaf
+
+    mount(partitions, leaf)
+  })
 
 class AdGroup extends React.Component {
   static displayName = 'AdGroup'
@@ -22,8 +37,18 @@ class AdGroup extends React.Component {
   }
 
   componentDidUpdate () {
-    if (this.state.open && !this.partitionsReady()) {
-      this.loadPartitions()
+    this.check()
+  }
+
+  check () {
+    if (!this.state.open) return
+
+    if (!this.partitionsReady()) {
+      return this.loadPartitions()
+    }
+
+    if (!this.state.tree) {
+      this.mountTree()
     }
   }
 
@@ -31,8 +56,21 @@ class AdGroup extends React.Component {
     return Boolean(this.props.adGroup.partitions)
   }
 
-  toggle = () => {
-    this.setState({open: !this.state.open})
+  mountTree () {
+    const {partitions} = this.props.adGroup
+    const tree = make(find(partitions, {parent: null}))
+
+    mount(partitions, tree)
+
+    this.setState({tree})
+  }
+
+  onClickToggle = e => {
+    e.preventDefault()
+
+    this.setState({
+      open: !this.state.open
+    })
   }
 
   loadPartitions = once(() =>
@@ -43,11 +81,15 @@ class AdGroup extends React.Component {
   render () {
     return (
       <div>
-        <a onClick={this.toggle}>
-          <i className='material-icons'>keyboard_arrow_right</i>
+        <a href='' onClick={this.onClickToggle}>
+          <i className='material-icons'>
+            keyboard_arrow_right
+          </i>
         </a>
 
-        <strong>{this.props.adGroup.name}</strong>
+        <strong>
+          {this.props.adGroup.name}
+        </strong>
       </div>
     )
   }
